a626c3dc7d702e3dae77da84d8624b6c8eb9c611
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2021-02-18 15:31:23 -0600

    create fakeLogin function for frontend fetch request and build create and show methods in user controller to respond to fetch request

--- book-collection-backend/app/controllers/api/v1/users_controller.rb
+++ book-collection-backend/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,26 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def create
+        # binding.pry
+        if user = User.find_by(name: user_params[:name])
+            redirect_to "/api/v1/users/#{user.id}"
+        else
+            user = User.create(user_params)
+            render json: user
+        end
+        
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
--- book-collection-frontend/src/index.js
+++ book-collection-frontend/src/index.js
@@ -1,5 +1,27 @@
 console.log("hello")
-const BACKEND_URL = 'localhost:3000';
-fetch("http://localhost:3000/api/v1/users")
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+const BACKEND_URL = 'http://localhost:3000/api/v1/';
+
+document.addEventListener("DOMContentLoaded", function(){
+    fakeLogin()
+})
+
+function fakeLogin(){
+    let form = document.querySelector('#form')
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
+                    name: e.target.children[1].value
+                } 
+            })
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    })
+}
