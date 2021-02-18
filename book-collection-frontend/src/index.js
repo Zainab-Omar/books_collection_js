546acaa44aee25da2c19a5e8be38b2676610e150
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2021-02-18 13:41:59 -0600

    create note.md file and build a user form

--- book-collection-frontend/src/index.js
+++ book-collection-frontend/src/index.js
@@ -1,1 +1,5 @@
 console.log("hello")
+const BACKEND_URL = 'localhost:3000';
+fetch("http://localhost:3000/api/v1/users")
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
